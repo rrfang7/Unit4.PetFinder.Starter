9f12bb67ee98fa49a3818ca6abd3530cd139a23b
Ralph Fang <ralphrainierfang@yahoo.com>
2023-10-30 20:33:12 -0400

    Finised Pet workshop

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const app = express();
 const PORT = 8080;
 
 // GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
-
-});
+// serve up the public folder as static index.html file
+    app.use(express.static('public'));
 
 // hello world route
 app.get('/api', (req, res) => {
@@ -21,31 +19,39 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.json(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    if (pet) {
+        res.json(pet);
+    } else {
+        res.status(404).json({message: "Pet not found"})
+    }
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name // extract name from parameters
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+   if (pet) {
+    res.json(pet);
+   } else {
+    res.status(404).json({message: "Pet not found"})
+   }
 });
 
 app.listen(PORT, () => {
